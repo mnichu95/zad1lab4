74540b7b6bbcacfc655f0ba61c8822204552847d
mnichu95 <218252@student.pwr.edu.pl>
2016-01-14 19:39:35 +0100

    Update My_Swing.java

--- My_Swing.java
+++ My_Swing.java
@@ -13,6 +13,8 @@ import javax.swing.JTextField;
 import java.awt.TextArea;
 import java.awt.Color;
 import java.awt.Choice;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class My_Swing {
 
@@ -44,6 +46,7 @@ public class My_Swing {
 	private JTextField txtWriteId;
 	private JTextField txtWriteMessage;
 	private JTextField txtSelectPriority;
+	private JButton btnWriteCurrentQueue;
 
 	public My_Swing() {
 		initialize();
@@ -76,6 +79,7 @@ public class My_Swing {
 		choice.add("URGENT");
 
 		JButton btnSendingFifo = new JButton("Sending Fifo");
+		btnSendingFifo.setBackground(new Color(204, 153, 153));
 		btnSendingFifo.setBounds(0, 143, 171, 79);
 		btnSendingFifo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -85,10 +89,12 @@ public class My_Swing {
 						.valueOf(choice.getSelectedItem())));
 
 			}
+
 		});
 		frame.getContentPane().add(btnSendingFifo);
 
 		JButton btnSendingLifo = new JButton("Sending Lifo");
+		btnSendingLifo.setBackground(new Color(153, 153, 204));
 		btnSendingLifo.setBounds(429, 143, 171, 79);
 		btnSendingLifo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -118,32 +124,52 @@ public class My_Swing {
 			}
 		});
 		frame.getContentPane().add(btnReceiving);
-		
+
 		txtYourMessages = new JTextField();
 		txtYourMessages.setBackground(Color.PINK);
 		txtYourMessages.setEditable(false);
 		txtYourMessages.setText("Your Messages:");
-		txtYourMessages.setBounds(221, 282, 154, 20);
+		txtYourMessages.setBounds(33, 283, 154, 20);
 		frame.getContentPane().add(txtYourMessages);
 		txtYourMessages.setColumns(10);
-		
+
 		txtWriteId = new JTextField();
+		txtWriteId.setBackground(new Color(255, 255, 204));
 		txtWriteId.setText("Write id");
 		txtWriteId.setBounds(0, 0, 86, 20);
 		frame.getContentPane().add(txtWriteId);
 		txtWriteId.setColumns(10);
-		
+
 		txtWriteMessage = new JTextField();
+		txtWriteMessage.setBackground(new Color(255, 255, 153));
 		txtWriteMessage.setText("Write message");
 		txtWriteMessage.setBounds(0, 42, 86, 27);
 		frame.getContentPane().add(txtWriteMessage);
 		txtWriteMessage.setColumns(10);
-		
+
 		txtSelectPriority = new JTextField();
+		txtSelectPriority.setBackground(new Color(255, 255, 0));
 		txtSelectPriority.setText("Select priority");
 		txtSelectPriority.setBounds(0, 100, 86, 20);
 		frame.getContentPane().add(txtSelectPriority);
 		txtSelectPriority.setColumns(10);
+
+		btnWriteCurrentQueue = new JButton(
+				"Write current queue to file before receiving");
+		btnWriteCurrentQueue.setBackground(new Color(204, 255, 0));
+		btnWriteCurrentQueue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					PrintWriter save = new PrintWriter("yourQueue.txt");
+					save.println("Your queue " + queue.list);
+					save.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		btnWriteCurrentQueue.setBounds(296, 246, 294, 50);
+		frame.getContentPane().add(btnWriteCurrentQueue);
 
 	}
 }
